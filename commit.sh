2f801628495862dcf2cf632c4fd068a8865944b0
elmikegonzalez <mgonzalez@amplience.com>
2018-11-20 18:56:19 -0800

    Added font color, added overlay color

--- commit.sh
+++ commit.sh
@@ -5,6 +5,6 @@ git add .
 echo 'Enter the commit message:'
 read commitMessage
 git commit -m "$commitMessage"
-echo 'Enter the name of the branch:'
-read branch
-git push origin $branch
+# echo 'Enter the name of the branch:'
+#echo read branch
+git push origin master
